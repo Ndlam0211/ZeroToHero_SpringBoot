e6f335831dc558cbe54c41e0fa14629bc0601c88
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2026-03-30 11:48:52 +0700

    Implement User API Endpoints

---
+++ src/main/java/com/lamnd/zerotohero/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.lamnd.zerotohero.controller;
+
+import com.lamnd.zerotohero.dto.request.UserCreationRequest;
+import com.lamnd.zerotohero.dto.request.UserUpdationRequest;
+import com.lamnd.zerotohero.entity.User;
+import com.lamnd.zerotohero.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getAllUsers() {
+        return userService.getAllUser();
+    }
+
+    @GetMapping("/{userId}")
+    ResponseEntity<?> getUserById(@PathVariable("userId") String userId) {
+        try {
+            User user = userService.getUserById(userId);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>("User Not Found",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{userId}")
+    ResponseEntity<?> updateUserById(@PathVariable("userId") String userId, @RequestBody UserUpdationRequest request) {
+        try {
+            User user = userService.updateUserById(userId,request);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>("User Not Found",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    ResponseEntity<?> deleteUserById(@PathVariable("userId") String userId) {
+        userService.deleteUserById(userId);
+        return new ResponseEntity<>("User Deleted Successfully", HttpStatus.NO_CONTENT);
+    }
+}
--- src/main/java/com/lamnd/zerotohero/service/UserService.java
+++ src/main/java/com/lamnd/zerotohero/service/UserService.java
@@ -1,17 +1,57 @@
 package com.lamnd.zerotohero.service;
 
 import com.lamnd.zerotohero.dto.request.UserCreationRequest;
+import com.lamnd.zerotohero.dto.request.UserUpdationRequest;
 import com.lamnd.zerotohero.entity.User;
 import com.lamnd.zerotohero.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-    public User createRequest(UserCreationRequest request){
+    public User createUser(UserCreationRequest request){
+        User user = new User();
 
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepo.save(user);
+    }
+
+    public List<User> getAllUser() {
+        return userRepo.findAll();
+    }
+
+    public User getUserById(String userId) {
+        return findUserById(userId);
+    }
+
+    public User updateUserById(String userId, UserUpdationRequest request) {
+        User user = findUserById(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepo.save(user);
+    }
+
+    public void deleteUserById(String userId) {
+        userRepo.deleteById(userId);
+    }
+
+    private User findUserById(String userId) {
+        return userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found")
+        );
     }
 }
